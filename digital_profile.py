b7a6af60e0e8445bfd3dca051ad96bc10b07dd22
415-29-napasorn <56788@yupparaj.ac.th>
2026-06-25 09:02:05 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My Digital Profile")
+
+st.header("แนะนำตัว")
+st.write("นางสาวนภสร แสงสุวรรณ์")
+st.write("ชื่อเล่น ยิปซี")
+st.write("เลขที่ 29 ม.4/15")
+
+st.header("เป้าหมายและสื่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย: อยากพูดได้มากกว่า3ภาษา")
+st.write("สิ่งที่อยากเป็นในอนาคต: ล่าม (Interpreter)")
