85f5bb754ac4edd16dc942e305ae8d55bccb085a
415-29-napasorn <56788@yupparaj.ac.th>
2026-09-03 08:55:19 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,6 +101,6 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวนภสร แสงสุวรรณ์ ม.4/15 เลขที่ 29")
 
 
